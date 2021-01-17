c8e3a8bca8f4131cbace71ef536ce608f3849413
Diego <jhowzeraDiego@gmail.com>
2021-01-16 22:42:03 -0300

    add comentarios

--- lib/view/splash.dart
+++ lib/view/splash.dart
@@ -1,7 +1,11 @@
+//Imports
 import 'dart:async';
-import 'package:betfire/view/principal.dart';
 import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
+
+//telas
+import 'package:betfire/view/principal.dart';
+import 'package:betfire/auth/login.dart';
 
 class Splash extends StatefulWidget {
   @override
@@ -12,6 +16,8 @@ class _State extends State<Splash> {
   @override
   void initState() {
     super.initState();
+
+    //timer com o tempo para chamar a rota
     Timer.periodic(Duration(seconds: 3), (timer) {
       setState(() {
         route();
@@ -19,16 +25,19 @@ class _State extends State<Splash> {
       timer.cancel();
     });
   }
-
+  
+  //rota de destino depois do timer
   route() {
     Navigator.pushReplacement(context,
-        PageTransition(child: Principal(), type: PageTransitionType.bottomToTop));
+        PageTransition(child: Login(), type: PageTransitionType.bottomToTop));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.orange[700],
+
+      //Imagem na Parte central da tela
       body: Center(
         child: Image.asset(
           "imagens/logoBetFire.png",
