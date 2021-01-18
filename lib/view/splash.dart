7e99561b90ba2d53f17ec91623013bdf0e0f40ca
Diego <jhowzeraDiego@gmail.com>
2021-01-18 14:48:01 -0300

    add icone da inicializacao

--- lib/view/splash.dart
+++ lib/view/splash.dart
@@ -4,8 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
 
 //telas
-import 'package:betfire/view/principal.dart';
-import 'package:betfire/auth/login.dart';
+import 'package:betfire/nav.dart';
 
 class Splash extends StatefulWidget {
   @override
@@ -29,7 +28,7 @@ class _State extends State<Splash> {
   //rota de destino depois do timer
   route() {
     Navigator.pushReplacement(context,
-        PageTransition(child: Login(), type: PageTransitionType.bottomToTop));
+        PageTransition(child: Nav(), type: PageTransitionType.bottomToTop));
   }
 
   @override
